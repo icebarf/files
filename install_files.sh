a0419e8c4397e7fa095f24319680174300e278e9
icebarf <sysgrammer@protonmail.com>
2022-06-20 16:38:28 +0530

    add install script for files

---
+++ install_files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# set zsh as default shell
+chsh -s $(command -v zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install rest of the config files
+cp -r .config/alacritty $HOME/
+cp -r {.emacs.d,.vim,Pictures,.local} $HOME/
+cp -r {.vimrc,.zshrc,.zprofile} $HOME/
