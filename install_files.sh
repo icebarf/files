5de1a227d361fa874a10186ccec88f49ecb8e147
Amritpal <sysgrammer@protonmail.com>
2022-07-01 14:53:33 +0530

    Update install_files.sh

--- install_files.sh
+++ install_files.sh
@@ -7,6 +7,6 @@ chsh -s $(command -v zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install rest of the config files
-cp -r .config/alacritty $HOME/
+cp -r .config/alacritty $HOME/.config/
 cp -r {.emacs.d,.vim,Pictures,.local} $HOME/
 cp -r {.vimrc,.zshrc,.zprofile} $HOME/
